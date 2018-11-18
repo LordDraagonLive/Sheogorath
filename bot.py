c561275874c24d3f82a3fe5de6c32b0a8c6d658d
LordDraagonLive <buddhiadikari@live.com>
2018-11-18 08:23:56 +0000

    Initial commit

---
+++ bot.py
@@ -0,0 +1,4 @@
+import discord
+# print(discord.__version__) # check to make sure at least once you're on the right version!
+token = open("config.ini", "r").read()  # I've opted to just save my token to a text file. 
+print(token)
